c233c9efefa8108115e4ab107f8db9d10ee772b1
Phyllis Kan <pyk9q7@gmail.com>
2019-07-20 13:59:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,7 @@ def third_challenge
         #your key/value pairs here
         :name => "Romeo",
         :age => "15",
-        :staus => "alive"
+        :status => "alive"
         },
       :hero_friends => []
    },
