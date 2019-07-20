551b046ecd2b7bd3d690076f0216ec79d897f791
Phyllis Kan <pyk9q7@gmail.com>
2019-07-20 14:10:40 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,7 +1,3 @@
-epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
-
-puts epic_tragedy
-
 def bonus
   epic_tragedy = {
    :montague => {
@@ -25,7 +21,9 @@ def bonus
   }
 
   #code your solution here:
-
+  epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
+   
+  puts epic_tragedy
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
